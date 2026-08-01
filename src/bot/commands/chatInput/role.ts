c00f21092142506da34089354468eca823aca580
mloetta <arthurmpcarmo@gmail.com>
2026-08-01 07:40:54 -0300

    \n

--- src/bot/commands/chatInput/role.ts
+++ src/bot/commands/chatInput/role.ts
@@ -71,7 +71,7 @@ createApplicationCommand({
             },
             {
               type: ComponentType.TextDisplay,
-              content: `${emoji('Calendar')} **Created At:**\n${timestamp(getTimestampFromSnowflake(role.id), TimestampStyle.LongDate)} (${timestamp(getTimestampFromSnowflake(role.id), TimestampStyle.RelativeTime)})\n> Hoisted: **${role.hoist ? 'Yes' : 'No'}**\n> Mentionable: **${role.mentionable ? 'Yes' : 'No'}**\n> Managed: **${role.managed ? 'Yes' : 'No'}**\n> Position: **${role.position}**\n> Colors: **#${role.colors.primary_color.toString(16).padStart(6, '0')}${role.colors.secondary_color ? `, #${role.colors.secondary_color.toString(16).padStart(6, '0')}` : ''}${role.colors.tertiary_color ? `, #${role.colors.tertiary_color.toString(16).padStart(6, '0')}` : ''}**\n> Permissions: **${shownPermissions.join(', ') || 'None'}**${extraPermissions > 0 ? ` \`+${extraPermissions}\`` : ''}`,
+              content: `${emoji('Calendar')} **Created At:**\n${timestamp(getTimestampFromSnowflake(role.id), TimestampStyle.LongDate)} (${timestamp(getTimestampFromSnowflake(role.id), TimestampStyle.RelativeTime)})\n\n> Hoisted: **${role.hoist ? 'Yes' : 'No'}**\n> Mentionable: **${role.mentionable ? 'Yes' : 'No'}**\n> Managed: **${role.managed ? 'Yes' : 'No'}**\n> Position: **${role.position}**\n> Colors: **#${role.colors.primary_color.toString(16).padStart(6, '0')}${role.colors.secondary_color ? `, #${role.colors.secondary_color.toString(16).padStart(6, '0')}` : ''}${role.colors.tertiary_color ? `, #${role.colors.tertiary_color.toString(16).padStart(6, '0')}` : ''}**\n> Permissions: **${shownPermissions.join(', ') || 'None'}**${extraPermissions > 0 ? ` \`+${extraPermissions}\`` : ''}`,
             },
           ],
         },
