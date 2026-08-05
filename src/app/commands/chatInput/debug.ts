131fa052e22bfe079ebcd198eb9adc8988ad85fd
mloetta <arthurmpcarmo@gmail.com>
2026-08-05 19:13:04 -0300

    typo of doom and despair

--- src/app/commands/chatInput/debug.ts
+++ src/app/commands/chatInput/debug.ts
@@ -37,7 +37,7 @@ createApplicationCommand({
             },
             {
               type: ComponentType.TextDisplay,
-              content: `> Commands: **${commands.size}**\n> Components: **${components.size}**\n> Installs: **${app.approximate_user_install_count}**\n> Uptime: **${msToReadableTime(process.uptime() * 1000)} (${timestamp(Math.floor(new Date().getTime() - process.uptime() * 1000), TimestampStyle.ShortDate)})**7`,
+              content: `> Commands: **${commands.size}**\n> Components: **${components.size}**\n> Installs: **${app.approximate_user_install_count}**\n> Uptime: **${msToReadableTime(process.uptime() * 1000)} (${timestamp(Math.floor(new Date().getTime() - process.uptime() * 1000), TimestampStyle.ShortDate)})**`,
             },
             {
               type: ComponentType.Separator,
